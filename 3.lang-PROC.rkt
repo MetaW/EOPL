b584f37308f6f4ccdb86baedf7eac84935450a2a
wll <iwanglulu@gmail.com>
2016-07-17 20:25:54 +0800

    update lang-LET,lang-PROC; create lang-LETREC

--- 3.lang-PROC.rkt
+++ 3.lang-PROC.rkt
@@ -1,6 +1,6 @@
 #lang eopl
 
-;language-LET : concrete & abstract syntax tree
+;language-PROC : concrete & abstract syntax tree
 ;----------------------------------------------------
 ;concrete syntax tree:
 
@@ -12,6 +12,8 @@
 ;              | if Expression then Expression else Expression
 ;              | Identifer
 ;              | let Identifer = Expression in Expression
+;              | proc (Idenfifer) Expression
+;              | (Expression Expression)
 
 
 ; abstract syntax tree
@@ -29,7 +31,12 @@
   (var-exp (exp symbol?))
   (let-exp (var symbol?)
            (exp expression?)
-           (body expression?)))
+           (body expression?))
+  (proc-exp (var symbol?)
+            (body expression?))
+  (call-exp (operator expression?)
+            (operand expression?)))
+
 
 
 ; scan&parse
@@ -37,7 +44,7 @@
 (define scanner-spec
   '((whitespace (whitespace) skip)
     (comment (";" (arbno (not #\newline))) skip)
-    (identifer (letter (arbno letter digit "-" "_" "?")) symbol)
+    (identifer (letter (arbno (or letter digit "-" "_" "?"))) symbol)
     (number (digit (arbno digit)) number)
     (number ("-" digit (arbno digit)) number)))
 ;   (class (regexp) action)
@@ -51,7 +58,9 @@
    (expression ("zero?" "(" expression ")") zero?-exp)
    (expression ("if" expression "then" expression "else" expression) if-exp)
    (expression (identifer) var-exp)
-   (expression ("let" identifer "=" expression "in" expression) let-exp)))
+   (expression ("let" identifer "=" expression "in" expression) let-exp)
+   (expression ("proc" "(" identifer ")" expression) proc-exp)
+   (expression ("(" expression expression ")") call-exp)))
 ;  (algebric-data-type (concrete-syntax-with-class/string/ADT) constructor)
 ; class/string : terminator
 ; ADT : nonterminator
@@ -64,22 +73,40 @@
 
 ; expressed valus <-> denoted value
 ;----------------------------------------------------
+
+; datatype proc is not necessary
+(define-datatype proc proc?
+  (procedure (var symbol?)
+             (body expression?)
+             (en env?)))
+
 (define-datatype expval expval?
   (num-val (num number?))
-  (bool-val (bool boolean?)))
+  (bool-val (bool boolean?))
+  (proc-val (proc proc?)))
 
 
+; extractor : expval -> number
 (define expval->num
   (lambda (val)
     (cases expval val
       (num-val (num) num)
       (else (eopl:error "expval extract error in expval->num: ~s" val)))))
 
+; extractor : expval -> bool
 (define expval->bool
   (lambda (val)
     (cases expval val
       (bool-val (bool) bool)
       (else (eopl:error "expval extract error in expval->bool: ~s" val)))))
+
+; extractor : expval -> proc
+(define expval->proc
+  (lambda (val)
+    (cases expval val
+      (proc-val (proc) proc)
+      (else (eopl:error "expval extract error in expval->bool: ~s" val)))))
+
 
 
 ; environment
@@ -101,7 +128,7 @@
      (empty-env)))))
 
 
-; apply-env : symbol -> expval
+; apply-env : symbol * env -> expval
 (define apply-env
   (lambda (var en)
     (cases env en
@@ -114,6 +141,14 @@
 
 ; eval
 ;----------------------------------------------------
+
+; auxiliary function for value-of
+; apply-procdure : proc * expval -> expval
+(define apply-procedure
+  (lambda (proc0 val)
+    (cases proc proc0
+      (procedure (var body en) (value-of body (extend-env var val en))))))
+
 
 ; value-of : Exp * Env -> ExpVal
 (define value-of
@@ -137,7 +172,12 @@
       (let-exp (var exp body) (value-of body
                                         (extend-env var
                                                     (value-of exp env)
-                                                    env))))))
+                                                    env)))
+      (proc-exp (param body) (proc-val (procedure param body env)))
+      (call-exp (procid exp)
+                (let ((proci (expval->proc (value-of procid env)))
+                      (param (value-of exp env)))
+                  (apply-procedure proci param))))))
 
 
 ; value-of-program : Program -> Expval
@@ -156,14 +196,12 @@
 
 ; test
 ;----------------------------------------------------
-;> (run "-(2,5)")
-;  #(struct:num-val -3)
 
-;> (run "let x = 3 in -(x,1)")
-;  #(struct:num-val 2)
+;> (run "let f = proc (x) -(x,10) in (f 20)")
+;  #(struct:num-val 10)
 
-;> (run "let x = 3 in let y = 2 in -(x,y)")
-;  #(struct:num-val 1)
+;> (run "let f = proc (x) proc (y) -(x,y) in ((f 5) 3)")
+; #(struct:num-val 2)
 
-;> (run "let x = 3 in let y = 2 in -(y,x)")
-;  #(struct:num-val -1)
+;> (run "let x = 200 in let f = proc (z) -(z,x) in let x = 100 in let g = proc (z) -(z,x) in -((f 1), (g 1))")
+;  #(struct:num-val -100)
